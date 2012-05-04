c4468fc34412bc0b77ede245a6de0ec6dfbb1c54
Andrew Stewart <andrew@averagestudios.com>
2012-05-04 09:32:47 -0700

    Improved the speed of the Chrome extension.
    Changed run_at event to document_start.

--- ext/dotcss.js
+++ ext/dotcss.js
@@ -2,8 +2,8 @@ $.ajax({
   url: 'http://localhost:1243/' + window.location.host.replace('www.', '') + '.css',
   dataType: 'text',
   success: function (data) {
-    var body = $("body");
-    $('<style></style>').appendTo(body).text(data);
+    var head = $("head");
+    $('<style></style>').appendTo(head).text(data);
   },
   error: function() {
     console.log("No dotcss server found at localhost:1243");
