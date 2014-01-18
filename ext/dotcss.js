fdc10ff030be80b2fbd3ed73b9a3668ca9c9cd8f
Heinrich Fenkart <hnrch02@gmail.com>
2014-01-18 23:15:00 +0100

    Remove jQuery dependency

--- ext/dotcss.js
+++ ext/dotcss.js
@@ -1,11 +1,16 @@
-$.ajax({
-  url: 'http://localhost:1243/' + window.location.host.replace('www.', '') + '.css',
-  dataType: 'text',
-  success: function (data) {
-    var head = $("head");
-    $('<style></style>').appendTo(head).text(data);
-  },
-  error: function() {
-    console.log("No dotcss server found at localhost:1243");
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://localhost:1243/' + window.location.host.replace(/^www\./, '') + '.css');
+
+xhr.onreadystatechange = function() {
+  if(xhr.readyState !== 4) {
+    return;
+  } else if(xhr.status !== 200) {
+    throw new Error('No dotcss server found at localhost:1243');
   }
-});
+
+  var style = document.createElement('style');
+  style.textContent = xhr.responseText;
+  document.head.appendChild(style);
+};
+
+xhr.send();
